c2378c0810cf6a98e1e9d2e93f9a42c33b5d90ee
Shanu <Shanu@Shanu-PC>
2018-12-17 17:43:54 +0530

    Changes

--- Aarry/src/StringReverse.java
+++ Aarry/src/StringReverse.java
@@ -4,7 +4,8 @@ public class StringReverse {
 
 	public static void main(String[] args) {
 	      StringBuffer a = new StringBuffer("Java programming is fun");
-      System.out.println(a.reverse());
+	      
+	      System.out.println(a.reverse());
 	 /*public static void main(String args[])
 	   {
 	      String original="honor", 
